dfb8f609ecdd8ec8f2d282120166acaee83c94bd
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-12 18:47:04 -0300

    novidade

--- MeuPrimeiroProjeto/app.js
+++ MeuPrimeiroProjeto/app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express()
 
+app.set('view engine', 'ejs');
+
+app.get('/home', (req, res) => {
+    
+})
+
 app.get('/', function(req, res){
-    res.send('Aló, Mundo!');
+    res.json({
+        nome: 'Lucas de souza santos',
+        idade: 28,
+        peso: 82.4
+    });
 });
 
 app.listen(3000, function(){
