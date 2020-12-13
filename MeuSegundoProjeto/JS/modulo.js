9c461554375fb7814dd52059e302a0df84bfd055
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-13 11:16:08 -0300

    novo curso

---
+++ MeuSegundoProjeto/JS/modulo.js
@@ -0,0 +1,19 @@
+function soma(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function multi(a, b){
+    return a * b
+}
+
+function div(a, b){
+    if (b != 0){
+        return a / b;
+    }else{
+        return "Divisão por zero";
+    }
+}
